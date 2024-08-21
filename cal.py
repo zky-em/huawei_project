8ae65bdfe425024adb196d25f6dbce8a38dc02bc
zky_git_417 <1274439249@qq.com>
2024-08-21 19:36:27 +0800

    days

--- cal.py
+++ cal.py
@@ -1,18 +1,14 @@
 import sys
 
 def add_numbers(numbers):
-    total = sum(numbers)
-    return total
+    return sum(numbers)
 
 def minus_numbers(numbers):
-    result = numbers[0]
-    for num in numbers[1:]:
-        result -= num
-    return result
+    return numbers[0] - sum(numbers[1:])
 
 def multiply_numbers(numbers):
-    result = numbers[0]
-    for num in numbers[1:]:
+    result = 1
+    for num in numbers:
         result *= num
     return result
 
@@ -23,9 +19,32 @@ def divide_numbers(numbers):
     return result
 
 def read_numbers_from_file(filename):
-    with open(filename, 'r') as file:
-        numbers = [float(line.strip()) for line in file]
-    return numbers
+    try:
+        with open(filename, 'r') as file:
+            numbers = [float(line.strip()) for line in file]
+        return numbers
+    except FileNotFoundError:
+        print(f"文件 '{filename}' 不存在")
+    except ValueError:
+        print(f"文件 '{filename}' 的格式不正确")
+
+def calculate(command, filename):
+    numbers = read_numbers_from_file(filename)
+    if numbers:
+        if command == "-add":
+            result = add_numbers(numbers)
+            print("加法计算结果：", result)
+        elif command == "-minus":
+            result = minus_numbers(numbers)
+            print("减法计算结果：", result)
+        elif command == "-multiply":
+            result = multiply_numbers(numbers)
+            print("乘法计算结果：", result)
+        elif command == "-divide":
+            result = divide_numbers(numbers)
+            print("除法计算结果：", result)
+        else:
+            print("无效的命令")
 
 def main():
     if len(sys.argv) != 3:
@@ -34,25 +53,8 @@ def main():
 
     command = sys.argv[1]
     filename = sys.argv[2]
-
-    if command == "-add":
-        numbers = read_numbers_from_file(filename)
-        result = add_numbers(numbers)
-        print("加法计算结果：", result)
-    elif command == "-minus":
-        numbers = read_numbers_from_file(filename)
-        result = minus_numbers(numbers)
-        print("减法计算结果：", result)
-    elif command == "-multiply":
-        numbers = read_numbers_from_file(filename)
-        result = multiply_numbers(numbers)
-        print("减法计算结果：", result)
-    elif command == "-divide":
-        numbers = read_numbers_from_file(filename)
-        result = divide_numbers(numbers)
-        print("减法计算结果：", result)
-    else:
-        print("无效的命令")
+    calculate(command, filename)
 
 if __name__ == "__main__":
     main()
+
